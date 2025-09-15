ab525e2fab15416d8691b46bc22026fbdd309a35
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-15 20:00:52 -0300

    Initial commit

---
+++ api-gestao-contas/src/main/java/br/com/fiap/sprint/api_gestao_contas/ApiGestaoContasApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sprint.api_gestao_contas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestaoContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestaoContasApplication.class, args);
+	}
+
+}
---
+++ api-gestao-contas/src/test/java/br/com/fiap/sprint/api_gestao_contas/ApiGestaoContasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.sprint.api_gestao_contas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestaoContasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
